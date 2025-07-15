faac15b2fa3dec6b5c0ded06fc806f287146c793
ArihantSrivastava2225/ <arihantsrivastava71011@gmail.com>
2025-07-15 20:15:23 +0530

    Initial-commit

---
+++ Awwards_Web/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import { ScrollTrigger, SplitText } from 'gsap/all'
+import { gsap } from 'gsap'
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
